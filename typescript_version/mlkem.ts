974465e9c57fbc2a2f8582bb1d31c38b7ba7767a
David Buchanan <d@vidbuchanan.co.uk>
2024-01-13 23:47:31 +0000

    multiplication works

--- typescript_version/mlkem.ts
+++ typescript_version/mlkem.ts
@@ -36,6 +36,8 @@ class Zq {
 		return new Zq((Q + this.n - Zq.cast(other).n) % Q);
 	}
 
+	// Zq elements should be 12-bit values, yielding 24-bit multiplication results, prior to reduction.
+	// These values should all fit in JS's fp64 numbers, without precision issues.
 	mul(other: Zq | number): Zq {
 		return new Zq((this.n * Zq.cast(other).n) % Q);
 	}
@@ -91,7 +93,7 @@ class Rq {
 		}
 
 		// reduction loop
-		for (let i=0; i<256; i++) {
+		for (let i=0; i<255; i++) {
 			c[i] = c[i].sub(c[i+256]);
 			// c[i+256] is implicitly zeroed
 		}
@@ -138,10 +140,16 @@ class Tq {
 		return new Tq(this.f.map((e, i) => e.sub(other.f[i])))
 	}
 
-	// in the NTT domain, multiplication is trivial
-	// XXX: TODO: not this trivial!!!
+	// in the NTT domain, multiplication is relatively trivial
 	mul(other: Tq): Tq {
-		return other;
+		const c: Zq[] = [];
+		for (let i=0; i<256; i += 2) {
+			const [a0, a1] = this.f.slice(i, i+2);
+			const [b0, b1] = other.f.slice(i, i+2);
+			c.push(a0.mul(b0).add(a1.mul(b1).mul(GAMMA[i/2])));
+			c.push(a0.mul(b1).add(a1.mul(b0)));
+		}
+		return new Tq(c);
 	}
 
 	nttInv(): Rq {
@@ -172,27 +180,11 @@ function bitRev7(n: number): number {
 	assert((n >= 0) && (n < (1<<7)));
 
 	// 8-bit rev bithack
-	n = ((n & 0xaa) >> 1) | ((n & 0x55) << 1);
-	n = ((n & 0xcc) >> 2) | ((n & 0x33) << 2);
-	n = ((n & 0xf0) >> 4) | ((n & 0x0f) << 4);
+	n = ((n & 0b10101010) >> 1) | ((n & 0b01010101) << 1);
+	n = ((n & 0b11001100) >> 2) | ((n & 0b00110011) << 2);
+	n = ((n & 0b11110000) >> 4) | ((n & 0b00001111) << 4);
 
 	return n >> 1; // shift off 0th bit (giving us a 7-bit rev overall)
-}
-
-// raise a number to a power, mod Q, using square-and-multiply
-// this is NOT constant-time, but it's also only used in LUTs
-function powQ(base: number, exp: number): number {
-	// TODO: range checks on args?
-	// `base` and `exp` should be 12-bit values, yielding 24-bit multiplication results.
-	// these values should all fit in JS's fp64 numbers, without precision issues
-	let val = 1; // "multiplicative identity element" (!)
-	for (let i=11; i>=0; i--) { // read bits MSB to LSB
-		val = (val * val) % Q; // square
-		if ((exp >> i) & 1) {
-			val = (val * base) % Q; // conditional multiply
-		}
-	}
-	return val;
 }
 
 // indexing these LUTs won't be a sidechannel because the index is not secret
@@ -258,11 +250,21 @@ function byteDecode(d: number, bytes: Uint8Array): number[] {
 }
 
 const mynumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255];
-const nttres = Rq.cast(mynumbers).ntt();
+const foo = Rq.cast(mynumbers);
+const bar = foo.add(foo);
+
+console.log(foo.mul(bar));
+
+const foontt = foo.ntt();
+const barntt = bar.ntt();
+const nttres = foontt.mul(barntt);
+console.log(nttres.nttInv())
+
+/*
 console.log(nttres);
 console.log(nttres.nttInv());
 const mybytes = byteEncode(12, mynumbers);
 console.log(mybytes);
 console.log(byteDecode(12, mybytes))
 
-console.log(new Zq(123));
+console.log(new Zq(123));*/
